0d672d1c2b383d27773574bf77b2b187bd441901
parkhyeonji <iamphj3@naver.com>
2023-05-18 11:32:17 +0900

    chore : 라우터 설정

---
+++ src/Router.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
+import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <RecoilRoot>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </Suspense>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <div>랜딩페이지</div>;
+};
+
+export default LandingPage;
