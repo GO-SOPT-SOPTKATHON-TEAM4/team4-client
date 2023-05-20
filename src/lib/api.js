9b78a2d850943ed8c6e6ee337be52f631437ac58
parkhyeonji <iamphj3@naver.com>
2023-05-20 23:14:14 +0900

    chore : 에러 처리 추가

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,38 @@
 import { client } from './axios';
 
-const getWorldcupList = async () => {
-  const { data } = await client.get('/api/posts/tournament');
-  if (data.status === 200) {
-    return data.data.posts;
+export const getWorldcupList = async () => {
+  try {
+    const { data } = await client.get('/api/posts/tournament');
+    if (data.status === 200) {
+      return data.data.posts;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getRanking = async () => {
+  try {
+    const { data } = await client.get('/api/posts/ranking');
+    if (data.status === 200) {
+      return data.data.posts;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const postUpload = async (image, nickname, comment) => {
+  try {
+    const { data } = await client.post('/api/posts', image, nickname, comment, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    if (data.status === 200) {
+      return data.data.posts;
+    }
+  } catch (e) {
+    console.error(e);
   }
 };
