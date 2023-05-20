d5eec76c9c8181a3eb4c51fe5e2967409154378a
HeeJungPark <phj7214na@naver.com>
2023-05-20 21:30:49 +0900

    feat: 전체적 구조

--- src/pages/WinnerPage.jsx
+++ src/pages/WinnerPage.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
-import Winner from '../components/Winner/Winner';
+import styled from 'styled-components';
+import 기안84 from '../assets/기안84.png';
 
 const WinnerPage = () => {
+
+    
   return (
     <>
-      위너페이지
-      <Winner />
+      <St.WinnerWrapper>
+        <St.WinnerHeader> 우승했어요 ! </St.WinnerHeader>
+        <St.WinnerImage src ={기안84} />
+        <St.Ranking> 
+            <button type='button'>
+              랭킹 보기
+            </button>
+        </St.Ranking>
+        <St.ShareMyImage>
+            <button type='button'>
+              내 사진 공유하기
+            </button>
+        </St.ShareMyImage>
+      </St.WinnerWrapper>
     </>
   );
 };
 
 export default WinnerPage;
+
+const St = {
+    WinnerWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    height: 100%; 
+
+    `,
+
+    WinnerHeader : styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    `,
+
+    WinnerImage : styled.img`
+    display: flex;
+    width: 30rem;
+    `,
+
+    Ranking : styled.section`
+    display: flex;
+    `,
+
+    ShareMyImage : styled.section`
+    display: flex;
+    `
+
+  };
+  
