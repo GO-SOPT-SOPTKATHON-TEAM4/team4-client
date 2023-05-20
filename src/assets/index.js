13a74b000364b0ee6c612132d00e3a5012022a68
parkhyeonji <iamphj3@naver.com>
2023-05-21 01:31:07 +0900

    fix : IcError 제거

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,3 @@ export { ReactComponent as IcProgress1 } from './ic_progress_1.svg'; // svg expo
 export { ReactComponent as IcProgress2 } from './ic_progress_2.svg'; // svg export 에시
 export { ReactComponent as IcProgress3 } from './ic_progress_3.svg'; // svg export 에시
 export { ReactComponent as IcProgress4 } from './ic_progress_4.svg'; // svg export 에시
-export { ReactComponent as IcError } from './ic_error.svg';
