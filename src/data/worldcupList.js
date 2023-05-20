31cf92ddb47eb7b86978678e7f4b11bd7a429665
parkhyeonji <iamphj3@naver.com>
2023-05-20 20:55:06 +0900

    chore : 월드컵 리스트 더미데이터 추가

--- src/data/worldcupList.js
+++ src/data/worldcupList.js
@@ -1,1 +1,52 @@
-const WORLDCUP_LIST = [{}];
+import { Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8 } from '../assets/img';
+
+const WORLDCUP_LIST = [
+  {
+    id: 1,
+    url: Img1,
+    nickname: '닉넴1',
+    comment: '1번사진 한줄설명~~',
+  },
+  {
+    id: 2,
+    url: Img2,
+    nickname: '닉넴2',
+    comment: '2번사진 한줄설명~~',
+  },
+  {
+    id: 3,
+    url: Img3,
+    nickname: '닉넴3',
+    comment: '3번사진 한줄설명~~',
+  },
+  {
+    id: 4,
+    url: Img4,
+    nickname: '닉넴4',
+    comment: '4번사진 한줄설명~~',
+  },
+  {
+    id: 5,
+    url: Img1,
+    nickname: '닉넴5',
+    comment: '5번사진 한줄설명~~',
+  },
+  {
+    id: 6,
+    url: Img6,
+    nickname: '닉넴6',
+    comment: '6번사진 한줄설명~~',
+  },
+  {
+    id: 7,
+    url: Img7,
+    nickname: '닉넴7',
+    comment: '7번사진 한줄설명~~',
+  },
+  {
+    id: 8,
+    url: Img8,
+    nickname: '닉넴8',
+    comment: '8번사진 한줄설명~~',
+  },
+];
