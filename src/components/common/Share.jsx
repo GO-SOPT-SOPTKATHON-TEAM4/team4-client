0a5a60c8cf5c5edf13ad4474ee8d6d8980df2e48
parkhyeonji <iamphj3@naver.com>
2023-05-21 07:00:16 +0900

    feat : 공유 페이지 구현

--- src/components/common/Share.jsx
+++ src/components/common/Share.jsx
@@ -1,9 +1,11 @@
 import { React, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import { RANKING_DATA } from '../../data/worldcupList';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import belowFruitImg from '../../assets/belowFruitImg.svg';
 import { getPostDetail } from '../../lib/api';
 import styled from 'styled-components';
+import 기안84 from '../../assets/기안84.png';
 
 const Share = () => {
   const [detailInfo, setDetailInfo] = useState({});
@@ -12,37 +14,43 @@ const Share = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   try {
-    //     const result = await getPostDetail(postId);
-    //     setDetailInfo(result);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // };a
+    const getDetailData = async () => {
+      try {
+        const result = await getPostDetail(postId);
+        console.log(result);
+        setDetailInfo(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    // fetchData();
-    setDetailInfo(RANKING_DATA);
+    getDetailData();
   }, [postId]);
   console.log(detailInfo);
 
+  const handleCopyUrl = () => {
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false);
+    }, 2000);
+  };
+
   return (
     <St.RankingDetailWrapper>
-      <h1>
-        공유페이지
-        <br />
-        {detailInfo.nickname}
-      </h1>
-      <p>{detailInfo.comment}</p>
+      <St.DetailWrappper>
+        <St.DisplayImg src={detailInfo.imageUrl} alt={detailInfo.comment} />
+        <St.Comment>{detailInfo.comment}</St.Comment>
+        <St.NickName>{detailInfo.nickname}</St.NickName>
+        <St.Vote
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          투표하러 가기
+        </St.Vote>
+      </St.DetailWrappper>
 
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        투표하러가기
-      </button>
+      <St.belowImg src={belowFruitImg} />
     </St.RankingDetailWrapper>
   );
 };
@@ -54,14 +62,102 @@ const St = {
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     gap: 2rem;
 
     & > h1 {
       font-size: 2rem;
     }
   `,
+
+  DetailWrappper: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin-top: 4rem;
+    margin-bottom: 0.9rem;
+
+    width: 34.3rem;
+    height: 48.6rem;
+
+    background: #ffffff;
+    box-shadow: 0rem 0.3rem 1rem rgba(0, 0, 0, 0.05);
+    border-radius: 1.6rem;
+  `,
+
+  Comment: styled.h1`
+    margin-top: 2rem;
+    width: 31.1rem;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+
+    text-align: center;
+    letter-spacing: -0.5px;
+  `,
+
   DisplayImg: styled.img`
-    width: 100%;
-    height: 30rem;
+    width: 31.1rem;
+    height: 23.4rem;
+    margin-top: 2.4rem;
+    border-radius: 0.8rem;
+  `,
+
+  NickName: styled.h2`
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+
+    text-align: center;
+    letter-spacing: -0.5px;
+
+    margin-top: 1.2rem;
+  `,
+
+  Vote: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 6.2rem;
+    width: 29.6rem;
+    margin-top: 4.2rem;
+    background: #ffe470;
+    border: none;
+    border-radius: 42px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 16px;
+    text-align: center;
+  `,
+
+  belowImg: styled.img`
+    margin-bottom: 1.6rem;
+  `,
+
+  ToastMessage: styled.div`
+    position: fixed;
+    bottom: 5rem;
+    opacity: 0.9;
+    background-color: #333;
+    color: #fff;
+    padding: 1.4rem 2.6rem;
+    border-radius: 2.4rem;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: -0.5px;
   `,
 };
